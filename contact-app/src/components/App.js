932ca291441505db4926b9854793df78df6c5b8e
pamboss13 <pambossvr@gmail.com>
2022-12-16 21:55:09 +0200

    2nd

---
+++ contact-app/src/components/App.js
@@ -0,0 +1,48 @@
+
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { v4 as uuid } from "uuid";
+import './App.css';
+import Header from './Header';
+import AddContact from './AddContact';
+import ContactList from './ContactList';
+
+function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts]=useState([]);
+
+  const addContactHandler=(contact)=>{
+    console.log(contact);
+    setContacts([...contacts, { id: uuid(), ...contact }]);
+  };
+
+  const removeContactHandler = (id)=>{
+    const newContactList = contacts.filter((contact)=>{
+      return contact.id !== id;
+    });
+
+    setContacts(newContactList);
+  }
+
+
+  useEffect(()=>{
+    const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(retrieveContacts)setContacts(retrieveContacts);
+  },[]);
+
+  useEffect(()=>{
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  },[contacts]);
+
+  
+  
+  return (
+    <div className="ui container">
+      <Header />
+      <AddContact addContactHandler={addContactHandler}/>
+      <ContactList contacts={contacts} getContactId={removeContactHandler}/>
+    </div>
+  );
+}
+
+export default App;
